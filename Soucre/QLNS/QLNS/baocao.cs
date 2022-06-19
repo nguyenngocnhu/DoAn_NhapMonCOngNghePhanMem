aa9acddcdeac33f37cd75ee28e08c63593086bf5
nguyenngocnhu <nguyenngocnhuy91tlt@gmail.com>
2022-06-19 16:36:53 +0700

    first commit

---
+++ Soucre/QLNS/QLNS/baocao.cs
@@ -0,0 +1,21 @@
+﻿namespace QLNS.DataSet1TableAdapters
+{
+}
+namespace QLNS {
+    
+    
+    public partial class baocao {
+        partial class DataTable1DataTable
+        {
+        }
+    }
+}
+
+namespace QLNS
+{
+
+
+    public partial class DataSet1
+    {
+    }
+}
